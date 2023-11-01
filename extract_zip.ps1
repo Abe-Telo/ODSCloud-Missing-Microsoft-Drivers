228277ce26c05220d2f3fc0b94dae79fae0e074f
thething <atillzcomputers@gmail.com>
2023-11-01 09:16:23 -0400

    Get-USBPartition 1 Instead of gussing

--- extract_zip.ps1
+++ extract_zip.ps1
@@ -1,8 +1,25 @@
+# Function to get USB partitions
+function Get-USBPartition {
+    Get-WmiObject Win32_DiskDrive | 
+        Where-Object { $_.MediaType -eq "Removable Media" } | 
+        ForEach-Object {
+            $_ | Get-WmiObject -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID='$($_.DeviceID)'} WHERE AssocClass=Win32_DiskDriveToDiskPartition" | 
+                ForEach-Object {
+                    $_ | Get-WmiObject -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID='$($_.DeviceID)'} WHERE AssocClass=Win32_LogicalDiskToPartition"
+                }
+        }
+}
+
+# Get USB drives
+$usbDrives = Get-USBPartition | Select-Object -ExpandProperty DeviceID
+
 # Load necessary assembly for zip operations and set paths
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 $zipUrl = "https://github.com/Abe-Telo/ODSCloud-Missing-Microsoft-Drivers/archive/refs/heads/main.zip"
-$basePath = "E:\OSDCloud\DriverPacks\"
+
+# Assuming you want to use the first detected USB drive letter for your basePath
+$basePath = "$($usbDrives[1]):\OSDCloud\DriverPacks\"
 $zipPath = Join-Path $basePath "ODSCloud-Missing-Microsoft-Drivers-main.zip"
 
 # Ensure directory exists
