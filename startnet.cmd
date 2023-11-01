e47ba496148451a723dfebd2e6f1393db52cc251
thething <atillzcomputers@gmail.com>
2023-11-01 01:51:05 -0400

    Changing metheds. (TO BE CLEAND UP)

--- startnet.cmd
+++ startnet.cmd
@@ -21,33 +21,58 @@ Powershell -Command "$response = Invoke-WebRequest -Uri 'http://worldtimeapi.org
 date /t  & time /t
  
 
+:: Check for internet connection by pinging a known reliable IP
+ping -n 1 8.8.8.8 > NUL 2>&1
+
+if %errorlevel% equ 0 (
+    echo Internet connection detected. Executing remote script...
+    powershell -ExecutionPolicy Bypass -NoProfile -Command "Invoke-Expression (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Abe-Telo/ODSCloud-Missing-Microsoft-Drivers/main/start.ps1' -UseBasicParsing).Content"
+    goto end
+) else (
+    echo No internet connection. Searching for local script...
+    for %%X in (C D E) do (
+        if exist %%X:\OSDCloud\DriverPacks\ODSCloud-Missing-Microsoft-Drivers-main\start.ps1 (
+            echo Found local script at %%X:\OSDCloud\DriverPacks\ODSCloud-Missing-Microsoft-Drivers-main\start.ps1
+            powershell -ExecutionPolicy Bypass -NoProfile -File "%%X:\OSDCloud\DriverPacks\ODSCloud-Missing-Microsoft-Drivers-main\start.ps1"
+            goto end
+        )
+    )
+)
+
+:end
+echo Script finished. 
+
+
+
+
+
 :: The Concept: This script looks for the file name offline.ps1 locally in drive, E,D,C. If it does not find it, It will download it for the next offline run.
 :: If you ever need an update, All you need to do is be connected to the internet. 
 :: Offline.ps1 should have its own command for its updates, If you are online, Otherwise it uses the local known drivers. 
 
-SETLOCAL
+::SETLOCAL
 
-REM Define paths
-SET scriptPathE=E:\OSDCloud\DriverPacks\ODSCloud-Missing-Microsoft-Drivers-main\offline.ps1
-SET scriptPathD=D:\OSDCloud\DriverPacks\ODSCloud-Missing-Microsoft-Drivers-main\offline.ps1
-SET scriptPathC=C:\OSDCloud\DriverPacks\ODSCloud-Missing-Microsoft-Drivers-main\offline.ps1
-SET onlineScriptURL=https://raw.githubusercontent.com/Abe-Telo/ODSCloud-Missing-Microsoft-Drivers/main/online.ps1
+::REM Define paths
+::SET scriptPathE=E:\OSDCloud\DriverPacks\ODSCloud-Missing-Microsoft-Drivers-main\offline.ps1
+::SET scriptPathD=D:\OSDCloud\DriverPacks\ODSCloud-Missing-Microsoft-Drivers-main\offline.ps1
+::SET scriptPathC=C:\OSDCloud\DriverPacks\ODSCloud-Missing-Microsoft-Drivers-main\offline.ps1
+::SET onlineScriptURL=https://raw.githubusercontent.com/Abe-Telo/ODSCloud-Missing-Microsoft-Drivers/main/online.ps1
 
-REM Check for the existence of offline.ps1 in predefined paths and execute if found
-FOR %%P IN (%scriptPathE% %scriptPathD% %scriptPathC%) DO (
-    IF EXIST %%P (
-        ECHO Found offline.ps1 on: %%~dP
-        START powershell -NoProfile -ExecutionPolicy Bypass -File "%%P"
-        EXIT /B
-    )
-) 
+::REM Check for the existence of offline.ps1 in predefined paths and execute if found
+::FOR %%P IN (%scriptPathE% %scriptPathD% %scriptPathC%) DO (
+::    IF EXIST %%P (
+::        ECHO Found offline.ps1 on: %%~dP
+::        START powershell -NoProfile -ExecutionPolicy Bypass -File "%%P"
+::        EXIT /B
+::    )
+::) 
 
-REM If offline.ps1 is not found in any location, try downloading and executing online.ps1
-ECHO Script offline.ps1 not found in any location. Downloading...
-powershell -command "Invoke-Expression (Invoke-WebRequest -Uri '%onlineScriptURL%' -UseBasicParsing).Content"
-ECHO Finished executing the online script.
+::REM If offline.ps1 is not found in any location, try downloading and executing online.ps1
+::ECHO Script offline.ps1 not found in any location. Downloading...
+::powershell -command "Invoke-Expression (Invoke-WebRequest -Uri '%onlineScriptURL%' -UseBasicParsing).Content"
+::ECHO Finished executing the online script.
 
-ENDLOCAL
+::ENDLOCAL
    
 :: OSDCloud related commands
 powershell -NoL -C Initialize-OSDCloudStartnet
