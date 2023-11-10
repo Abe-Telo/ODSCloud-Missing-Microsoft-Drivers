ce816c1f7de15119f31814cc9c51c59e0c129e69
thething <atillzcomputers@gmail.com>
2023-11-09 23:15:13 -0500

    Update online.ps1

--- online.ps1
+++ online.ps1
@@ -114,6 +114,7 @@ if ($sku -eq "Surface_3_Nag") {
     Write-Host "Model Found: $model" -ForegroundColor Yellow
     Write-Host "This model is not yet investigated. Please report any issues in WinPE" -ForegroundColor red
     #Invoke-Expression (Invoke-WebRequest -Uri $SurfacePro -UseBasicParsing).Content
+    exit
 
 } elseif ($model -eq "Surface Pro 2") { 
     Write-Host "Model Found: $model" -ForegroundColor Yellow
