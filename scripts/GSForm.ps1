982965724aa08bdce9a11a5b02e8c81450103b28
thething <atillzcomputers@gmail.com>
2023-11-22 21:27:07 -0500

    Update GSForm.ps1

--- scripts/GSForm.ps1
+++ scripts/GSForm.ps1
@@ -1,11 +1,11 @@
 # With this script, you can log Serial numbers And other system info, Into a google sheet. This works with Google Forms.
 # To set this up correctly, Create a google form from drive.google.com
 # Create and name your 7 fields. EG: S/N, SKU, Model, Win Ver, CPU, Ram, HD.
-# Create a file "GSFormURL.txt" and past the URL.
+# Create a file "GSFormsConfig.txt" and past the URL.
 # Click on the three dots and lick, Get pre filed link. Right click and click inspect. 
 # Find <input name="entry.123456789"> Change fields for each entry.
 # Run the script .\GSForm.ps1 and test it. 
-
+ 
 # Define the log file path
 $logFilePath = "$PSScriptRoot\GSFormLog.txt"
 
@@ -16,11 +16,21 @@ function Write-Log {
     $message | Out-File -FilePath $logFilePath -Append
 }
 
+# Read configuration from GSFormsConfig.txt
+$configFilePath = "$PSScriptRoot\GSFormsConfig.txt"
+$configData = @{}
+Get-Content $configFilePath | ForEach-Object {
+    $parts = $_ -split '='
+    $key = $parts[0]
+    $value = $parts[1].Trim()
+    $configData[$key] = $value
+}
+
 # Fetch system information
 Write-Log "Fetching system information..."
 $serialNumber = (Get-WmiObject -Class Win32_BIOS).SerialNumber
-$sku = (Get-ComputerInfo).CsSystemSkuNumber
 $model = (Get-WmiObject -Class Win32_ComputerSystem).Model
+$sku = (Get-ComputerInfo).CsSystemSkuNumber
 $windowsVersion = (Get-ComputerInfo).WindowsProductName + " " + (Get-ComputerInfo).WindowsVersion
 $cpu = (Get-WmiObject -Class Win32_Processor).Name
 $ram = [Math]::Round((Get-ComputerInfo).CsTotalPhysicalMemory / 1GB)
@@ -36,28 +46,21 @@ Write-Log "CPU: $cpu" -color 'Green'
 Write-Log "RAM: ${ram}GB" -color 'Green'
 Write-Log "HD: ${hd}GB" -color 'Green'
 
-# Define the URL of the Google Form submission endpoint
-Write-Log "Retrieving the Google Form URL..."
-$GSForm = Get-Content -Path "$PSScriptRoot\GSFormURL.txt"
-
-# Log the URL
-Write-Log "Google Form URL: $GSForm"
+# Define the URL of the Google Form submission endpoint and form data
+$GSForm = $configData["GoogleFormURL"]
+$formData = @{
+    $configData["serialNumber"] = $serialNumber
+    $configData["model"] = $model
+    $configData["sku"] = $sku
+    $configData["windowsVersion"] = $windowsVersion
+    $configData["cpu"] = $cpu
+    $configData["ram"] = $ram
+    $configData["hd"] = $hd
+}
 
 # Create a new web request session object
 Write-Log "Creating a new web request session..."
 $session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
-
-# Form data to be submitted
-Write-Log "Preparing form data for submission..."
-$formData = @{
-    "entry.366548140" = $serialNumber
-    "entry.446059738" = $model
-    "entry.155658717" = $sku
-    "entry.730980097" = $windowsVersion
-    "entry.732944140" = $cpu
-    "entry.362455697" = $ram
-    "entry.1305266274" = $hd
-}
 
 # Submit the form
 Write-Log "Submitting the form data..."
